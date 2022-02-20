432a513e6d5955d43b6f27c0fdb8ade16a005137
Iago Freitas <iagofreitas05@gmail.com>
2022-02-20 15:30:09 -0300

    1 - class course model

---
+++ nep-pib-mogi/app/Actions/Courses/CreateNewCourse.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Validator;
+use Laravel\Jetstream\Jetstream;
+use App\Models\Course;
+
+class CreateNewCourse
+{
+    public function create (array $input){
+        Validator::make($input, [
+            'name' => ['required', 'string', 'max:255'],
+            'description' => ['required', 'string'],
+            'duration' => ['required']
+        ])->validate();
+
+        return Course::create([
+
+        ]);
+    }
+}
