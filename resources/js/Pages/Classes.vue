4bce091cb56078401ab0a9423d9cdbdf015dddf5
Iago Freitas <iagofreitas05@gmail.com>
2022-03-29 19:55:34 -0300

    yeah

---
+++ resources/js/Pages/Classes.vue
@@ -0,0 +1,58 @@
+<template>
+    <app-layout title="Cursos">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                nome do curso
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                area de aprendizagem
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+import AppLayout from '@/Layouts/AppLayout.vue'
+import Welcome from '@/Jetstream/Welcome.vue'
+import JetNavLink from '@/Jetstream/NavLink.vue'
+import swal from "sweetalert";
+
+export default defineComponent({
+    components: {
+        AppLayout,
+        Welcome,
+        JetNavLink,
+    },
+    data(){
+        return {
+            subscriptionForm: this.$inertia.form({
+              course_id : ''
+            })
+        }
+    },
+    props: ['courses'],
+    methods: {
+        confirmSubscription(id) {
+            swal({
+                title: "Você tem certeza?",
+                text: "gostaria de realizar a sua inscrição nesse curso?!",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+            })
+                .then((willDelete) => {
+                    this.subscriptionForm.course_id = id
+                    if (willDelete) {
+                        this.subscriptionForm.post(this.route('subscription'));
+                    } else {
+                        swal("Inscrição cancelada!");
+                    }
+                });
+        }
+    }
+})
+</script>
