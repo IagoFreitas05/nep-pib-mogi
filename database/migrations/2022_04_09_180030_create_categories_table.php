6f96abf56628ae5ceafda020b591aa698cc50241
Iago Freitas <iagofreitas05@gmail.com>
2022-04-09 15:16:05 -0300

    bugfix

--- database/migrations/2022_04_09_180030_create_categories_table.php
+++ database/migrations/2022_04_09_180030_create_categories_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('category');
             $table->string('description');
-            $table->string('');
+            $table->boolean('status');
             $table->timestamps();
         });
     }
