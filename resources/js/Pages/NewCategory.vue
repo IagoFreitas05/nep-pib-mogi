970e4e7768a64c15bb0816f10503fe519168f6b8
Iago Freitas <iagofreitas05@gmail.com>
2022-04-11 22:28:12 -0300

    adding

---
+++ resources/js/Pages/NewCategory.vue
@@ -0,0 +1,68 @@
+<template>
+    <app-layout title="Adicionar nova categoria">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Adicionar novo curso do NEP
+            </h2>
+        </template>
+
+        <div class="w-full mt-4 sm:max-w-xl  mx-auto p-2 mt-2 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg   ">
+            <form @submit.prevent="">
+                <div>
+                    <jet-label for="name" value="Nome do curso" />
+                    <jet-input id="name" type="text" class="mt-1 block w-full" v-model="form.name" required autofocus autocomplete="name" />
+                    <div v-if="errors.name">{{ errors.name }}</div>
+                </div>
+
+                <div class="mt-4">
+                    <jet-label for="duration" value="Duração do curso" />
+                    <jet-input id="duration" type="text" class="mt-1 block w-full" v-model="form.duration" required />
+                    <div v-if="errors.duration">{{ errors.duration }}</div>
+                </div>
+
+                <div class="mt-4">
+                    <jet-label for="description" value="Descrição" />
+                    <jet-input id="description" type="text" class="mt-1 block w-full" v-model="form.description" required />
+                </div>
+                <div class="flex items-center justify-end mt-4">
+
+                    <jet-button class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                        adicionar
+                    </jet-button>
+                </div>
+            </form>
+        </div>
+    </app-layout>
+</template>
+<script>
+import {defineComponent} from 'vue'
+import JetAuthenticationCard from '@/Jetstream/AuthenticationCard.vue'
+import JetAuthenticationCardLogo from '@/Jetstream/AuthenticationCardLogo.vue'
+import JetButton from '@/Jetstream/Button.vue'
+import JetInput from '@/Jetstream/Input.vue'
+import JetCheckbox from '@/Jetstream/Checkbox.vue'
+import JetLabel from '@/Jetstream/Label.vue'
+import JetValidationErrors from '@/Jetstream/ValidationErrors.vue'
+import {Head, Link} from '@inertiajs/inertia-vue3';
+import AppLayout from '@/Layouts/AppLayout.vue'
+
+export default {
+    components: {
+        Head,
+        JetAuthenticationCard,
+        JetAuthenticationCardLogo,
+        JetButton,
+        JetInput,
+        JetCheckbox,
+        JetLabel,
+        JetValidationErrors,
+        Link,
+        AppLayout
+    },
+    name: "NewCategory"
+}
+</script>
+
+<style scoped>
+
+</style>
