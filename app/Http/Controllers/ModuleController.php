a8664106ca81b2e385c1ed491ca21a48296a1f32
Iago Freitas <iagofreitas05@gmail.com>
2022-04-30 14:16:47 -0300

    feature: config moduleController

--- app/Http/Controllers/ModuleController.php
+++ app/Http/Controllers/ModuleController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Module;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ModuleController extends Controller
 {
-    //
+    public function create(Request $request){
+        return  Module::create([
+            'name' => $request->input('name'),
+            'description' => $request->input('description'),
+            'course_id' => $request->input('course_id')
+        ]);
+    }
 }
--- routes/module.php
+++ routes/module.php
@@ -1,4 +1,8 @@
 <?php
-    Route::middleware(['auth:sanctum', 'verified'])
-        ->post('/newModule', "App\Http\Controllers\ModuleController@Create")
-        ->name("newCategory");
+use App\Models\Category;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::middleware(['auth:sanctum', 'verified'])
+    ->post('/newModule', "App\Http\Controllers\ModuleController@Create")
+    ->name("newModule");
