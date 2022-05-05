f5afd706eb6bf2376a21fdca00f161f3c2c2c3ac
Iago Freitas <iagofreitas05@gmail.com>
2022-05-04 21:43:56 -0300

    t

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Quiz;
 use Illuminate\Http\Request;
 
 class QuizController extends Controller
 {
-    //
+    public function create(Request $request){
+        Quiz::create([
+
+        ]);
+    }
 }
