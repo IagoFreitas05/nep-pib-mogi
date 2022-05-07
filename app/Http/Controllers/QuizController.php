c4ac68040c014a97f123fb1824af9115b38af454
Iago Freitas <iagofreitas05@gmail.com>
2022-05-07 19:32:01 -0300

    new

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -2,13 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
+use App\Models\Module;
 use App\Models\Quiz;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class QuizController extends Controller
 {
     public function create(Request $request){
         Quiz::create([
+            "question" => $request->input('question'),
+            "order" => $request->input('order'),
+            "answer" => $request->input('answer'),
+            "module_id" => $request->input('module_id')
+        ]);
+
+        return Inertia::render('EditCourse', [
+            'course' => Course::find(),
+            'classes' => Course::find(Module::find('module_id')->course_id)->classCourse()->orderBy('class_order')->get()
 
         ]);
     }
--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -10,7 +10,7 @@ class Quiz extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['question','order','answer'];
+    protected $fillable = ['question','order','answer','module_id'];
 
     public function module(): BelongsTo{
         return $this->belongsTo(Module::class);
