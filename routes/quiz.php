c4a2306731c9ebd4a56de988770744bfd779fb0b
Iago Freitas <iagofreitas05@gmail.com>
2022-05-09 20:01:27 -0300

    feature: all quizzes

--- routes/quiz.php
+++ routes/quiz.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Models\Quiz;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum', 'verified'])
     ->post('/newQuiz', "App\Http\Controllers\QuizController@Create")
     ->name("newQuiz");
+
+Route::middleware(['auth:sanctum', 'verified'])
+    ->post('/index', function(){
+        return Quiz::All();
+    })->name("indexQuiz");
