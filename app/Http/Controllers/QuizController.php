91cf5787cba33ef13b0fd725823d2a2c10156fc2
Iago Freitas <iagofreitas05@gmail.com>
2022-05-16 10:53:45 -0300

    first commit on monday

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -20,11 +20,20 @@ class QuizController extends Controller
 
         return Inertia::render('EditCourse', [
             'course' => Course::find(),
-            'classes' => Course::find(Module::find('module_id')->course_id)->classCourse()->orderBy('class_order')->get()
+            'classes' => Course::find(Module::find($request->input('module_id'))->course_id)->classCourse()->orderBy('class_order')->get()
         ]);
     }
 
     public function update(Request $request){
+        $quiz = Quiz::find($request->input('id'));
+        $quiz->question = $request->input('question');
+        $quiz->order = $request->input('order');
+        $quiz->answer = $request->input('answer');
+        $quiz->save();
 
+        return Inertia::render('EditCourse', [
+            'course' => Course::find(),
+            'classes' => Course::find(Module::find($quiz->module_id)->course_id)->classCourse()->orderBy('class_order')->get()
+        ]);
     }
 }
