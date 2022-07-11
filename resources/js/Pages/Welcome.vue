aadf0f474a8e6a4d26f116c537ce429e01c34da4
Iago Freitas <iagofreitas05@gmail.com>
2022-07-11 13:35:57 -0300

    isso ai

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -2,8 +2,30 @@
     <Head title="Bem vindo"/>
 
     <div class="w-full">
-        <nav>
+        <nav class="flex flex-row bg-gray-100 p-4">
+            <jet-application-mark class=" object-cover object-center rounded"/>
 
+                <Link :href="route('login')"
+                      class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0
+                      py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg ">
+                    acessar
+                </Link>
+
+                <Link v-if="canRegister" :href="route('register')"
+                      class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0
+                      py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                    me cadastrar
+                </Link>
+                <Link v-if="$page.props.user" :href="route('dashboard')"
+                      class="inline-flex text-white bg-indigo-500 border-0 py-2
+                       px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                    Dashboard
+                </Link>
+            <Link v-if="$page.props.user" :href="route('dashboard')"
+                  class="inline-flex  text-white bg-indigo-500 border-0 py-2
+                       px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                sobre o projeto
+            </Link>
         </nav>
     </div>
 
